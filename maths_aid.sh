e27f54029feba98141af686b0de73b8738704c23
Skippy <samuelzheng11@gmail.com>
2017-07-27 11:18:51 +1200

    Added the introduction page file [starting the project]

---
+++ maths_aid.sh
@@ -0,0 +1,11 @@
+printf "===========================================================================\n"
+printf "Welcome to the Maths Authoring Aid\n"
+printf "===========================================================================\n"
+printf "Please select from one of the following options :\n"
+printf "\t(l)ist existing creations\n"
+printf "\t(p)lay and existing creation\n"
+printf "\t(d)elete and existing creation\n"
+printf "\t(c)reate a new creation\n"
+printf "\t(q)uit authoring tool\n"
+printf "Enter a selection [l//q//d//c//q]:\n"
+
