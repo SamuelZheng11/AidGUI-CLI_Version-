bff010741864b411cb3461da918e8e5fd447e6d6
Skippy <samuelzheng11@gmail.com>
2017-07-30 17:13:53 +1200

    added messages for all other options

--- maths_aid.sh
+++ maths_aid.sh
@@ -7,7 +7,7 @@ printf "\t(p)lay and existing creation\n"
 printf "\t(d)elete and existing creation\n"
 printf "\t(c)reate a new creation\n"
 printf "\t(q)uit authoring tool\n"
-printf "Enter a selection [l//q//d//c//q]: "
+printf "Enter a selection [l//q//d//c//q]: \n"
 
 listCreations="l"
 playCreations="p"
@@ -15,9 +15,34 @@ deleteCreations="d"
 createCreations="c"
 quit="q"
 
-read userSelection
-if [ "$userSelection" == "$listCreations" ];
-then
-  printf "\nthe Exisiting creations are :\n "
-fi
 
+loop=true
+while [  true ] 
+do
+        read userSelection
+	if [ "$userSelection" == "$listCreations" ];
+	then
+  		printf "the Exisiting creations are : \n "
+	elif [ "$userSelection" == "$playCreations" ];
+		then
+  		printf "Which creation do you wish to play? : \n "
+	elif [ "$userSelection" == "$deleteCreations" ];
+		then
+  		printf "Which creation do you want to delete? : \n "
+	elif [ "$userSelection" == "$createCreations" ];
+		then
+  		printf "What creation do you wish to make? : \n "
+	elif [ "$userSelection" == "$quit" ];
+		then
+		break;
+  		printf "Closing program\n ";
+	else 
+		printf "not a valid command please enter one of the selections provided above : \n";
+	fi
+done
+
+
+
+
+
+	
