7b836497706c3081f05883a9937ea7392f68721d
Samuel Zheng <szhe560@en-337213.uoa.auckland.ac.nz>
2017-07-31 17:18:31 +1200

    changed all the if/elseif/else statments to case/switch statements, added function calls instead of printing straigh to CLI

--- maths_aid.sh
+++ maths_aid.sh
@@ -1,45 +1,51 @@
-printf "===========================================================================\n"
-printf "Welcome to the Maths Authoring Aid\n"
-printf "===========================================================================\n"
-printf "Please select from one of the following options :\n"
-printf "\t(l)ist existing creations\n"
-printf "\t(p)lay and existing creation\n"
-printf "\t(d)elete and existing creation\n"
-printf "\t(c)reate a new creation\n"
-printf "\t(q)uit authoring tool\n"
-printf "Enter a selection [l//q//d//c//q]: \n"
+#!/bin/bash
 
-listCreations="l"
-playCreations="p"
-deleteCreations="d"
-createCreations="c"
-quit="q"
+printSetup()
+{
+	printf "===========================================================================\n"
+	printf "Welcome to the Maths Authoring Aid\n"
+	printf "===========================================================================\n"
+	printf "Please select from one of the following options :\n"
+	printf "\t(l)ist existing creations\n"
+	printf "\t(p)lay and existing creation\n"
+	printf "\t(d)elete and existing creation\n"
+	printf "\t(c)reate a new creation\n"
+	printf "\t(q)uit authoring tool\n"
+	printf "Enter a selection [l//q//d//c//q]: "
+}
+
+getUserCommand(){
+	read userSelection;
+	case $userSelection in
+	   l) printf "the Exisiting creations are : \n ";
+			getCreationsList
+			;;
+	
+	   p) printf "Which creation do you wish to play? : \n "
+			getPlayCreationOptions
+			;;
+	
+	   d) printf "Which creation do you want to delete? : \n "
+			getDeleteCreationOptions
+			;;
+	
+	   c) printf "What creation do you wish to make? : \n "
+			createCreation
+			;;
+	
+	   q) printf "Closing visual/audio aid program\n ";
+			exit
+			;;
+	
+	   *) printf "not a valid command please enter one of the selections provided above : \n";
+			getUserCommand
+			;;
+	esac
+}
 
 
-loop=true
-while [  true ] 
-do
-        read userSelection
-	if [ "$userSelection" == "$listCreations" ];
-	then
-  		printf "the Exisiting creations are : \n "
-	elif [ "$userSelection" == "$playCreations" ];
-		then
-  		printf "Which creation do you wish to play? : \n "
-	elif [ "$userSelection" == "$deleteCreations" ];
-		then
-  		printf "Which creation do you want to delete? : \n "
-	elif [ "$userSelection" == "$createCreations" ];
-		then
-  		printf "What creation do you wish to make? : \n "
-	elif [ "$userSelection" == "$quit" ];
-		then
-		break;
-  		printf "Closing program\n ";
-	else 
-		printf "not a valid command please enter one of the selections provided above : \n";
-	fi
-done
+printSetup;
+getUserCommand;
 
 
 
