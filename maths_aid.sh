0f22c1dfd0417291dcdc8fe58111e421bd6fdeb8
Skippy <samuelzheng11@gmail.com>
2017-08-05 01:10:43 +1200

    Got the entire createCreation Function Working, its 1:11 am 5.8.17 ;(

--- maths_aid.sh
+++ maths_aid.sh
@@ -1,77 +1,151 @@
 #/bin/bash
 
+declare -r localHome=`pwd`;
+
 printSetup()
 {
-	printf "===========================================================================\n"
-	printf "Welcome to the Maths Authoring Aid\n"
-	printf "===========================================================================\n"
-	printf "Please select from one of the following options :\n"
-	printf "\t(l)ist existing creations\n"
-	printf "\t(p)lay and existing creation\n"
-	printf "\t(d)elete and existing creation\n"
-	printf "\t(c)reate a new creation\n"
-	printf "\t(q)uit authoring tool\n"
-	printf "Enter a selection [l//q//d//c//q]: "
+	echo "==========================================================================="
+	echo "Welcome to the Maths Authoring Aid"
+	echo "==========================================================================="
+	mkdir "creations";
+}
+
+printMenu(){
+	cd "$localHome";
+	echo "";
+	echo "Please select from one of the following options :"
+	echo -e "\t(l)ist existing creations"
+	echo -e "\t(p)lay and existing creation"
+	echo -e "\t(d)elete and existing creation"
+	echo -e "\t(c)reate a new creation"
+	echo -e "\t(q)uit authoring tool"
+	getUserCommand
 }
 
 getUserCommand(){
-	read userSelection;
+	read -p "Enter a selection [l/q/d/c/q]: " -n1 userSelection;
+	echo "";
+	echo "";
 	case $userSelection in
-	   l) getCreationsList;
+	   [lL]) getCreationsList;
 			;;
 	
-	   p) getPlayCreationOptions;
+	   [pP]) getPlayCreationOptions;
 			;;
 	
-	   d) getDeleteCreationOptions;
+	   [dD]) getDeleteCreationOptions;
 			;;
 	
-	   c) createCreation;
+	   [cC]) createCreation;
 			;;
 	
-	   q) printf "Closing visual/audio aid program\n ";
+	   [qQ]) echo "Closing visual/audio aid program ";
+			echo "";
+			read -sp "Press any key to continue" -n1
+			echo "";
 			exit;
 			;;
 	
-	   *) printf "not a valid command please enter one of the selections provided above : \n";
-			getUserCommand
+	   *) echo "sorry I do not understand the command \""$userSelection"\". Please select from the option provided only ";
+			read -sp "Press any key to continue" -n1;
+			echo "";
+			printMenu;
 			;;
 	esac
 }
 
 getCreationsList(){
-	printf "the Exisiting creations are : \n ";
+	echo "the Exisiting creations are : ";
 	
 }
 
 GetPlayCreationOptions(){
-	printf "Which creation do you wish to play? : \n ";
+	echo "Which creation do you wish to play? : ";
 
 }
 
 getDeleteCreationOptions(){
-	printf "Which creation do you want to delete? : \n ";
+	echo "Which creation do you want to delete? : ";
 
 }
 
 createCreation(){
-	printf "What creation do you wish to make? : \n ";
-	read creationName;
-	if [`ls ~/Desktop/206/AidGUI-CLI_Version-/Audio | grep "$creationName.*" &> dev\null` == null]
-	then
-		printf "found nothing with $creationName\n";
-	else
-		printf "A file with that name already exists, please choose a new one\n";
-		createCreation;
-	fi
 
+	echo "";
+	read -p "Please type the name that you want the creation to be called : " creationName;
+	
+	if [ -d ./creations/"$creationName" ]
+	then
+		echo "";
+		echo "A file with that name already exists, Please choose another name";
+		echo "";
+		createCreation; 
+	else
+		mkdir ./creations/"$creationName"	
+	fi
+	
+	makeVideo "$creationName";
+}
+
+makeVideo(){
+
+	cd "$localHome"/creations/"$1";
+	#back slashes have been used in the ffmpeg command below to notify bash that i am not done
+	#with the command yet before going to the next line, i am working on a very small laptop screen :(
+	ffmpeg -f lavfi -i color=c=orange:s=320x240:d=3.0 -vf \
+	"drawtext=fontfile= "$localHome"/font/font.ttf:fontsize=30: \
+	fontcolor=blue:x=(w-text_w)/2:y=(h-text_h)/2:text='$1'" videoOnly.mp4 &> /dev/null
+	
+	recordVoice "$1";
+}
+
+recordVoice(){
+
+	echo ""	
+	echo "Please record yourself saying \""$1"\""
+	read -p"Press any key to start recording, you will have 3 seconds to record" -n1
+	echo ""
+	echo "[Recording ...]"
+	ffmpeg -f alsa -i hw:0 -t 3 audioOnly.wav &> /dev/null;
+	echo "Finished recording"
+	echo ""
+	read -sp"Would you like to listen to the recording? [y/n]" -n1 listenToRecording
+	echo ""
+	
+	case $listenToRecording in
+	
+		[yY]) ffplay -t 3 -autoexit audioOnly.wav &> /dev/null;
+			;;
+
+		[nN]) #do nothing
+			;;
+	esac
+
+	echo ""
+	read -sp"Is this acceptable? [y/n]" -n1 acceptable
+	echo ""
+
+		case $acceptable in
+	
+		[yY]) finishCreation "$1";
+			;;
+
+		[nN]) echo "" 
+				echo "ok we will re-record the audio clip"
+				rm -f audioOnly.wav &> /dev/null;
+				recordVoice "$1";
+			;;
+	esac
+}
+
+finishCreation(){
+
+	ffmpeg -i videoOnly.mp4 -i audioOnly.wav \
+	-c:v copy -c:a aac -strict experimental \
+	-map 0:v:0 -map 1:a:0 "$1".mp4 &> /dev/null
+
+	printMenu;
 }
 
 printSetup;
-getUserCommand;
-
-
-
-
-
-	
+printMenu;
